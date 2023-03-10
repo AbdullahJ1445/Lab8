66b7b48b554ca3c9e9039079592b0fa563a18b67
Abdullah Mohammed <37706742+AbdullahJ1445@users.noreply.github.com>
2023-03-10 15:20:39 -0700

    hasCity pass

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -64,4 +64,19 @@ public class CustomList extends ArrayAdapter<City> {
         cities.add(city);
     }
 
+    /**
+     * this checks if the city is in the list
+     * @param cityName
+     * @return
+     */
+
+    public boolean hasCity(String cityName){
+        for(City city : cities){
+            if(city.getCityName().equals(cityName)){
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
